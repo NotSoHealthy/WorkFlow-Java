40de89f542b079b366e9f53c7caec44a7f68dfd4
yssfmlha <123382121+yssfmlha@users.noreply.github.com>
2025-02-12 22:06:12 +0100

    deleted Amine branch test.java

--- src/main/java/com/PIDev3A18/projet/test.java
+++ src/main/java/com/PIDev3A18/projet/test.java
@@ -1,6 +1,5 @@
 package com.PIDev3A18.projet;
 
-<<<<<<< HEAD
 
 import entity.Reservation;
 import services.ServiceEmployee;
@@ -10,8 +9,6 @@ import services.ServiceReservation;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class test {
     public static void main(String[] args) throws SQLException {
@@ -28,85 +25,3 @@ public class test {
 
     }
 }
-=======
-import entity.Applications;
-import entity.Employee;
-import entity.JobOffer;
-import entity.Interviews;
-import services.ApplicationService;
-import services.InterviewService;
-import services.JobOfferService;
-import services.ServiceEmployee;
-
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-
-public class test {
-    public static void main(String[] args) {
-        try {
-            ServiceEmployee serviceEmployee= new ServiceEmployee();
-            Employee employee =serviceEmployee.readById(1);
-
-
-            JobOfferService jobService = new JobOfferService();
-            Date publicationDate = new Date();
-            Date expirationDate = new Date(System.currentTimeMillis() + 86400000L); // 1 day later
-            JobOffer jobOffer = new JobOffer(
-                    "Software Engineer 21344",
-                    "Develop and maintain software",
-                    publicationDate,
-                    expirationDate,
-                    "Full-Time",
-                    2400,
-                    employee
-            );
-            jobService.add(jobOffer);
-            System.out.println("Job Offer Added Successfully!");
-
-            List<JobOffer> jobOffers = jobService.readAll();
-
-
-
-
-            JobOffer jobOfferId = jobService.readById(2);
-
-            ApplicationService appService = new ApplicationService();
-            Applications application = new Applications(
-                    jobOfferId,
-                    employee,
-                    "CV_Pathsaddsa.pdf",
-                    "Cover Letter Text",
-                    new Date(),
-                    "Pending"
-              );
-            appService.add(application);
-            System.out.println("Application Added Successfully!");
-
-            List<Applications> applications = appService.readAll();
-            System.out.println("All Applications: " + applications);
-
-
-            Applications applicationId = appService.readById(4);
-
-            InterviewService interviewService = new InterviewService();
-            Interviews interview = new Interviews(
-                    applicationId,
-                    employee,
-                    new Date(),
-                    "Online",
-                    "Good Candidate",
-                    "Scheduled"
-            );
-            interviewService.add(interview);
-
-            List<Interviews> interviews = interviewService.readAll();
-            System.out.println("All Interviews: " + interviews);
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
->>>>>>> Amine
