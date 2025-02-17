aa9f924ee37b953e693e7820bd4b6ef20672c8a2
yssfmlha <123382121+yssfmlha@users.noreply.github.com>
2025-02-17 03:23:14 +0100

    Added Event updating

--- src/main/java/com/PIDev3A18/projet/EvenementsController.java
+++ src/main/java/com/PIDev3A18/projet/EvenementsController.java
@@ -23,10 +23,13 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class EvenementsController {
 
+    private Event ToUpdateEvent;
     @FXML
     private VBox eventHolder = null;
     @FXML
@@ -35,6 +38,8 @@ public class EvenementsController {
     private AnchorPane AddEvent;
     @FXML
     private AnchorPane EventDisplay;
+    @FXML
+    private AnchorPane UpdateEvent;
     @FXML
     private TextField Titre;
     @FXML
@@ -70,6 +75,40 @@ public class EvenementsController {
     @FXML
     private Label NbplaceError;
     @FXML
+    private TextField TitreUpdate;
+    @FXML
+    private TextArea DescriptionUpdate;
+    @FXML
+    private DatePicker DateUpdate;
+    @FXML
+    private TextField HeureUpdate;
+    @FXML
+    private TextField MinuteUpdate;
+    @FXML
+    private TextField AdresseUpdate;
+    @FXML
+    private ComboBox<String> TypeListUpdate;
+    @FXML
+    private TextField NbplaceUpdate;
+    @FXML
+    private ImageView backUpdate;
+    @FXML
+    private Label TitreErrorUpdate;
+    @FXML
+    private Label DescriptionErrorUpdate;
+    @FXML
+    private Label DateErrorUpdate;
+    @FXML
+    private Label HeureErrorUpdate;
+    @FXML
+    private Label MinuteErrorUpdate;
+    @FXML
+    private Label AdresseErrorUpdate;
+    @FXML
+    private Label TypeErrorUpdate;
+    @FXML
+    private Label NbplaceErrorUpdate;
+    @FXML
     public void initialize() {
         eventHolder.getChildren().clear();
         TypeList.setItems(FXCollections.observableArrayList("Workshop", "Commerce", "Conference" , "Webinaire" , "Networking" , "Reunion"));
@@ -94,6 +133,10 @@ public class EvenementsController {
                 controller.setTitle(event.getTitre());
                 controller.setDescription(event.getDescription());
                 controller.setEvent(event);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy, HH:mm", Locale.FRENCH);
+                String formattedDate = event.getDateetheure().format(formatter);
+                controller.setDateHeure(formattedDate);
+                controller.setAdresse("Adresse: "+event.getLieu());
                 controller.setController(this);
                 if(!loggedinEmployee.getType().equals("responsable")){
                     controller.setDeleteInvisible();
@@ -107,15 +150,49 @@ public class EvenementsController {
             i++;
         }
     }
+    public void setUpdateEvent(Event e) {
+        this.ToUpdateEvent = e;
+    }
+    public void setTitreUpdate(String titre) {
+        TitreUpdate.setText(titre);
+    }
+    public void setDescriptionUpdate(String description) {
+        DescriptionUpdate.setText(description);
+    }
+    public void setDateUpdate(LocalDate date) {
+        DateUpdate.setValue(date);
+    }
+    public void setHeureUpdate(String heure) {
+        HeureUpdate.setText(heure);
+    }
+    public void setMinuteUpdate(String minute) {
+        MinuteUpdate.setText(minute);
+    }
+    public void setAdresseUpdate(String adresse) {
+        AdresseUpdate.setText(adresse);
+    }
+    public void setTypeListUpdate(String type) {
+        TypeListUpdate.setValue(type);
+    }
+    public void setNbplaceUpdate(String nbplace) {
+        NbplaceUpdate.setText(nbplace);
+    }
     public void layoutGoToAddEvenements(ActionEvent actionEvent) {
         AddEvent.setVisible(true);
         EventDisplay.setVisible(false);
         back.setVisible(true);
     }
+    public void layoutGoToUpdateEvenement() {
+        EventDisplay.setVisible(false);
+        backUpdate.setVisible(true);
+        UpdateEvent.setVisible(true);
+    }
     public void layoutGoBack(javafx.scene.input.MouseEvent mouseEvent) {
         AddEvent.setVisible(false);
+        UpdateEvent.setVisible(false);
         EventDisplay.setVisible(true);
         back.setVisible(false);
+        backUpdate.setVisible(false);
         Titre.setText("");
         Description.setText("");
         Date.setValue(null);
@@ -124,6 +201,14 @@ public class EvenementsController {
         Adresse.setText("");
         TypeList.setValue(null);
         Nbplace.setText("");
+        TitreUpdate.setText("");
+        DescriptionUpdate.setText("");
+        DateUpdate.setValue(null);
+        HeureUpdate.setText("");
+        MinuteUpdate.setText("");
+        AdresseUpdate.setText("");
+        TypeListUpdate.setValue(null);
+        NbplaceUpdate.setText("");
     }
     public void AddEvenements(ActionEvent event) {
         boolean test=true;
@@ -203,6 +288,83 @@ public class EvenementsController {
             Adresse.setText("");
             TypeList.setValue(null);
             Nbplace.setText("");
+        }
+    }
+    public void UpdateEvenements(ActionEvent event) {
+        boolean test=true;
+        ServiceEvent se = new ServiceEvent();
+        LocalDate selectedDate=DateUpdate.getValue();
+        TitreErrorUpdate.setText("");
+        DescriptionErrorUpdate.setText("");
+        DateErrorUpdate.setText("");
+        HeureErrorUpdate.setText("");
+        MinuteErrorUpdate.setText("");
+        AdresseErrorUpdate.setText("");
+        TypeErrorUpdate.setText("");
+        NbplaceErrorUpdate.setText("");
+        if(TitreUpdate.getText().isBlank()){
+            TitreErrorUpdate.setText("le titre ne peut pas être vide");
+            test=false;
+        }
+        if(DescriptionUpdate.getText().isBlank()){
+            DescriptionErrorUpdate.setText("la description ne peut pas être vide");
+            test=false;
+        }
+        if(selectedDate == null || selectedDate.isBefore(LocalDate.now())){
+            DateErrorUpdate.setText("Date Invalide");
+            test=false;
+        }
+        int hour=0;
+        int minute=0;
+        if(HeureUpdate.getText().isBlank()){
+            HeureErrorUpdate.setText("l'Heure ne peut pas étre vide");
+            test=false;
+        }
+        else{
+            hour=Integer.parseInt(HeureUpdate.getText());
+            if(hour>23||hour<0){
+                HeureErrorUpdate.setText("Heure invalide");
+                test=false;
+            }
+        }
+        if(MinuteUpdate.getText().isBlank()){
+            MinuteErrorUpdate.setText("la Minute ne peut pas être vide");
+            test=false;
+        }
+        else{
+            minute=Integer.parseInt(MinuteUpdate.getText());
+            if(minute>59||minute<0){
+                MinuteErrorUpdate.setText("Minute invalide");
+            }
+        }
+        if(AdresseUpdate.getText().isBlank()){
+            AdresseErrorUpdate.setText("l'Adresse ne peut pas être vide");
+            test=false;
+        }
+        if(TypeListUpdate.getValue()==null){
+            TypeErrorUpdate.setText("sélectionner un type");
+            test=false;
+        }
+        if(NbplaceUpdate.getText().isBlank()){
+            NbplaceErrorUpdate.setText("donner le nombre de places");
+            test=false;
+        }
+        if(test==true) {
+            LocalDateTime DateAndTime = LocalDateTime.of(selectedDate, LocalTime.of(hour, minute));
+            Event e = new Event(ToUpdateEvent.getId(),TitreUpdate.getText(), DescriptionUpdate.getText(), DateAndTime, AdresseUpdate.getText(), TypeListUpdate.getValue(), Integer.parseInt(NbplaceUpdate.getText()), ToUpdateEvent.getEmployee());
+            se.update(e);
+            UpdateEvent.setVisible(false);
+            EventDisplay.setVisible(true);
+            backUpdate.setVisible(false);
+            initialize();
+            TitreUpdate.setText("");
+            DescriptionUpdate.setText("");
+            DateUpdate.setValue(null);
+            HeureUpdate.setText("");
+            MinuteUpdate.setText("");
+            AdresseUpdate.setText("");
+            TypeListUpdate.setValue(null);
+            NbplaceUpdate.setText("");
         }
     }
 
--- src/main/java/com/PIDev3A18/projet/EventController.java
+++ src/main/java/com/PIDev3A18/projet/EventController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import services.ServiceEvent;
 
+import java.time.LocalDate;
+
 public class EventController {
     Event e;
     @FXML
@@ -14,6 +16,10 @@ public class EventController {
 
     @FXML
     private Label Title;
+    @FXML
+    private Label DateHeure;
+    @FXML
+    private Label Adresse;
     @FXML
     private Button DeleteButton;
 
@@ -24,6 +30,12 @@ public class EventController {
     public void setDescription(String description) {
         Description.setText(description);
     }
+    public void setDateHeure(String dateHeure) {
+        DateHeure.setText(dateHeure);
+    }
+    public void setAdresse(String adresse) {
+        Adresse.setText(adresse);
+    }
     public void setEvent(Event event) {
         this.e = event;
     }
@@ -31,6 +43,21 @@ public class EventController {
         ServiceEvent se = new ServiceEvent();
         se.delete(this.e);
         c.initialize();
+    }
+    public void Update(ActionEvent event) {
+        c.setUpdateEvent(e);
+        c.setTitreUpdate(e.getTitre());
+        c.setDescriptionUpdate(e.getDescription());
+        LocalDate dateOnly=e.getDateetheure().toLocalDate();
+        c.setDateUpdate(dateOnly);
+        String hour = Integer.toString(e.getDateetheure().getHour());
+        String minute = Integer.toString(e.getDateetheure().getMinute());
+        c.setHeureUpdate(hour);
+        c.setMinuteUpdate(minute);
+        c.setAdresseUpdate(e.getLieu());
+        c.setTypeListUpdate(e.getType());
+        c.setNbplaceUpdate(Integer.toString(e.getNombredeplace()));
+        c.layoutGoToUpdateEvenement();
     }
     public void setController(EvenementsController controller) {
         this.c = controller;
